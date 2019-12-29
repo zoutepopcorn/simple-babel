cb5bf5c54cc0887d2a9f0ffa1dc5371dd63e5052
Your Name <you@example.com>
2019-12-29 10:54:40 +0100

    simple babel node setup

---
+++ src/hallo/index.js
@@ -0,0 +1,1 @@
+export const prut = "hallo";
---
+++ src/main.js
@@ -0,0 +1,14 @@
+import {  prut } from './hallo';
+
+class Test {
+  constructor() {}
+
+  tester() {
+    console.log("test");
+    
+  }
+}
+
+const test = new Test();
+console.log(prut);
+test.tester();
